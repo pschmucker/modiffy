287430db9f59d4e6c899e6224507afa0d15f1892
Philippe SCHMUCKER <139227+pschmucker@users.noreply.github.com>
2023-02-28 23:23:48 +0100

    Delete unused file

--- src/Config.tsx
+++
@@ -1,5 +0,0 @@
-import i18n from "./i18n"
-
-export const useTranslations = (language: string, translations: any) => {
-    i18n.addResourceBundle(language, 'translation', translations, true, true);
-}
